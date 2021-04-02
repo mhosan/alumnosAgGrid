a3c40aee1edb8d85d4ac602fe205bc8c43ff4128
marcelo <mhosan@gmail.com>
2021-04-02 19:20:54 -0300

    nueve

--- src/app/componentes/home/home.component.ts
+++ src/app/componentes/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -14,41 +14,59 @@ export class HomeComponent implements OnInit {
   public columnDefs;
   public rowData;
   public rowSelection;
-  public destino:string = "...";
+  public destino: string = "(seleccionar)";
 
-  constructor() { 
-    
+  constructor(private router: Router) {
+
   }
 
   ngOnInit(): void {
     this.columnDefs = [
-      { headerName: 'Id', field: 'id'},
-      { headerName: 'Institución', field: 'institucion', sortable: true},
-      { headerName: 'Curso', field: 'curso', sortable: true},
-      { headerName: 'Nivel', field: 'nivel', sortable: true}
+      { headerName: 'Id', field: 'id' },
+      { headerName: 'Institución', field: 'institucion', sortable: true },
+      { headerName: 'Curso', field: 'curso', sortable: true },
+      { headerName: 'Nivel', field: 'nivel', sortable: true }
     ];
     this.rowData = [
-      { id: 1, institucion: 'Ipap', curso: 'Intro al giserio', nivel: 'Inicial'},
-      { id: 2, institucion: 'Arba', curso: 'Intro al giserio', nivel: 'Inicial'},
-      { id: 3, institucion: 'Ipap', curso: 'Avanzamos con todo', nivel: 'Avanzado'},
-      { id: 4, institucion: 'Arba', curso: 'Avanzamos con todo', nivel: 'Avanzado'}
+      { id: 1, institucion: 'Ipap', curso: 'Intro al giserio', nivel: 'Inicial' },
+      { id: 2, institucion: 'Arba', curso: 'Intro al giserio', nivel: 'Inicial' },
+      { id: 3, institucion: 'Ipap', curso: 'Avanzamos con todo', nivel: 'Avanzado' },
+      { id: 4, institucion: 'Arba', curso: 'Avanzamos con todo', nivel: 'Avanzado' }
     ];
     this.rowSelection = 'single';
   }
 
-  onSelectionChanged(parametro){
+  onSelectionChanged(parametro) {
     // let selectedNodes = this.gridApi.getSelectedNodes();
     // let selectedData = selectedNodes.map(node => node.data);
     // alert(`Nodo seleccionado:\n${JSON.stringify(selectedData)}`); 
     let selectedRows = this.gridApi.getSelectedRows();
     let seleccion = selectedRows[0].id;
     //alert(seleccion);
-    this.destino = seleccion;
+    this.destino = seleccion.toString();
   }
-  // onClickIrAlCurso(){
+
+  onClickIrAlCurso() {
     
-  // }
-  onGridReady(params){
+    switch (this.destino) {
+      case "1":
+        this.router.navigate(['/alumnos1']);
+        break;
+      case "2":
+        this.router.navigate(['/alumnos2']);
+        break;
+      case "3":
+        this.router.navigate(['/alumnos3']);
+        break;
+      case "4":
+        this.router.navigate(['/alumnos4']);
+        break;
+
+      default:
+        break;
+    }
+  }
+  onGridReady(params) {
     this.gridApi = params.api;
     this.gridColumnApi = params.columnApi;
     // this.http
