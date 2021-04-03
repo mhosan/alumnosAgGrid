67ed3d7d27fea4a20a818cf6d8bd6eb63ad1d92d
marcelo <mhosan@gmail.com>
2021-04-03 18:32:35 -0300

    nada

--- src/app/componentes/alumnos-ipap2/alumnos-ipap2.component.ts
+++ src/app/componentes/alumnos-ipap2/alumnos-ipap2.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DatosService } from '../../servicios/datos.service';
 
 @Component({
   selector: 'app-alumnos-ipap2',
@@ -6,10 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./alumnos-ipap2.component.css']
 })
 export class AlumnosIpap2Component implements OnInit {
+  public gridApi;
+  public gridColumnApi;
+  public columnDefs;
+  public rowData;
+  public rowSelection;
 
-  constructor() { }
+  constructor(private datosService: DatosService) { }
 
   ngOnInit(): void {
+    this.columnDefs = [
+      { headerName: 'Nombre', field: 'firstname', sortable: true },
+      { headerName: 'Apellido', field: 'lastname', sortable: true },
+      { headerName: 'email', field: 'email', sortable: true }
+    ];
+    //this.rowData = [];
+    this.rowSelection = 'single';
   }
-
+  onSelectionChanged(parametro) {
+    // let selectedNodes = this.gridApi.getSelectedNodes();
+    // let selectedData = selectedNodes.map(node => node.data);
+    // alert(`Nodo seleccionado:\n${JSON.stringify(selectedData)}`); 
+    let selectedRows = this.gridApi.getSelectedRows();
+    let seleccion = selectedRows[0].id;
+    //alert(seleccion);
+    //this.destino = seleccion.toString();
+  }
+  onGridReady(params) {
+    this.gridApi = params.api;
+    this.gridColumnApi = params.columnApi;
+    
+    this.datosService.getDatosIpapAvanzado()
+      .subscribe(respuestaJson => {
+        this.rowData = respuestaJson[0];
+      });
+  }
 }
--- src/app/servicios/datos.service.ts
+++ src/app/servicios/datos.service.ts
@@ -10,7 +10,19 @@ export class DatosService {
   constructor(private http: HttpClient) { }
 
   getDatosArbaInicial(): Observable<any> {
-    let datos = this.http.get<any>("../assets/arbaInicial.json")
+    let datos = this.http.get<any>("../assets/arbaInicial.json");
+    return datos;
+  }
+  getDatosArbaAvanzado(): Observable<any> {
+    let datos = this.http.get<any>("../assets/arbaAvanzado.json");
+    return datos;
+  }
+  getDatosIpapInicial(): Observable<any> {
+    let datos = this.http.get<any>("../assets/ipapInicial.json");
+    return datos;
+  }
+  getDatosIpapAvanzado(): Observable<any> {
+    let datos = this.http.get<any>("../assets/ipapAvanzado.json");
     return datos;
   }
 }
